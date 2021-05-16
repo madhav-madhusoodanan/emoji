645d066db6921310e4efeefdb4c6464001ef3ec0
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-05-16 12:07:49 +0400

    Update and rename processor_1.js to script_1.js

---
+++ script_1.js
@@ -0,0 +1,17 @@
+// this script extracts data and makes a minified version of keyword-emoji list
+
+const fs = require("fs");
+const emoji = require("./emoji"); // array
+
+// create a JSON object
+var data = emoji.map((obj) => obj["char"]);
+
+// convert JSON object to string
+
+// write JSON string to a file
+fs.writeFile("emoji.array.js", data, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("JSON data is saved.");
+});
